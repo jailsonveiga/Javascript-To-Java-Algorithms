5239556e66846eb7a6cae904dfe6bbd00c8c34d2
jailsonveiga <jmdaveiga2012@gmail.com>
2021-12-25 22:34:38 -0500

    JavasCript - Edabit / Create a function that takes an integer and returns true if it's divisible by 100, otherwise return false.

---
+++ Challenge.js
@@ -0,0 +1,10 @@
+// Multiple of 100
+// https://edabit.com/challenge/qMr6wYGr6NaXAPQGF
+
+function divisible(num) {
+  return num % 100 == 0;
+}
+
+console.log(divisible(1));
+console.log(divisible(1000));
+console.log(divisible(100));
