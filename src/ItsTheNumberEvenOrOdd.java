406925afcf174b1f4ce1b0dcf15d423bc4a0523f
jailsonveiga <jmdaveiga2012@gmail.com>
2022-01-16 22:58:02 -0500

    Is the Number Even or Odd?

---
+++ src/ItsTheNumberEvenOrOdd.java
@@ -0,0 +1,18 @@
+//Is the Number Even or Odd?
+//https://edabit.com/challenge/umRwYETG7ogToZhSi
+
+public class ItsTheNumberEvenOrOdd {
+    public static void main(String[] args){
+        System.out.println("Is the number even or odd (3) -> " + isEvenOrOdd(3));
+        System.out.println("Is the number even or odd (126) -> " + isEvenOrOdd(126));
+        System.out.println("Is the number even or odd (19) -> " + isEvenOrOdd(19));
+    }
+    public static String isEvenOrOdd(int num) {
+        if(num % 2 == 0) {
+            return "even";
+        }
+        else{
+            return "odd";
+        }
+    }
+}
