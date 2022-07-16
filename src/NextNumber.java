5fe09d45a1fcb713ab31a027ff9bf02ad26be5af
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-15 22:51:35 -0400

    Return the next number from the integer passed "java"

--- src/NextNumber.java
+++ src/NextNumber.java
@@ -4,6 +4,13 @@ by +1 and returns the result */
 
 public class NextNumber {
     public static void main(String[] args) {
-        
+        System.out.println("Next number of - 0 - is: " + nextNumber(0));
+        System.out.println("Next number of - 9 - is: " + nextNumber(9));
+        System.out.println("Next number of - -3 - is: " + nextNumber(-3));
+    }
+
+    public static int nextNumber(int num) {
+        num = num + 1;
+        return num;
     }
 }
