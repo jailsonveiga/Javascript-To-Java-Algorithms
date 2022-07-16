2c945efc930a26e7ae906dd0caab4b8cc8064df7
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-16 13:35:31 -0400

    Initial Commit

---
+++ findtheindex.js
@@ -0,0 +1,3 @@
+/* Create a function that searchesd for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1 */
+
+function search(arr, item) {}
