636b875392f5fb62328daf9c2067bb1b217858f2
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-15 21:23:43 -0400

    Return the first element

---
+++ getfirstelement.js
@@ -0,0 +1,7 @@
+// Create a function that takes an array containing only numbers and return the first element
+
+function getFirstValue(arr) {
+  console.log("Get the first Element in an Array - " + arr[0]);
+}
+
+getFirstValue([2, 5, 3, 9, 0]);
