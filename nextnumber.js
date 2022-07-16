e984db0158b3b11a5f89596a4f1cb8fbae208e86
jailsonveiga <jmdaveiga2012@gmail.com>
2022-07-15 22:35:35 -0400

    Return the next number from the integer passed

--- nextnumber.js
+++ nextnumber.js
@@ -1,3 +1,10 @@
 /* Create a function that takes a number as an argument, increments the number ny +1 and returns the result */
 
-function addition(num) {}
+function addition(num) {
+  num = num + 1;
+  console.log("Next number is: " + num);
+}
+
+addition(0);
+addition(9);
+addition(-3);
